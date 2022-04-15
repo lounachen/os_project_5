86b2ece6ba1ca831e7acc939804dcbf82ee055b7
lchen9 <lchen9@nd.edu>
2022-04-15 00:39:24 -0700

    fs_debug: direct

--- fs.c
+++ fs.c
@@ -11,13 +11,10 @@
 #define INODES_PER_BLOCK   128
 #define POINTERS_PER_INODE 5
 #define POINTERS_PER_BLOCK 1024
-#define MEM_SET			   4000
+#define MEM_SET			   400000
 
 int MOUNTED = 0;
-<<<<<<< HEAD
-=======
 int FORMATTED = 0;
->>>>>>> aecb7a75e1836db40a4e44686fe7d9ea620cba64
 int BITMAP[MEM_SET]; 
 
 struct fs_superblock {
@@ -42,12 +39,6 @@ union fs_block {
 };
 
 union fs_block SUPER_BLOCK;
-<<<<<<< HEAD
-union fs_block INODE_BLOCKS[MEM_SET];
-
-
-=======
->>>>>>> aecb7a75e1836db40a4e44686fe7d9ea620cba64
 
 int fs_format() {
 
@@ -69,41 +60,25 @@ int fs_format() {
 		ninodeblocks += 1;
 	}
 
-<<<<<<< HEAD
-	// clear the inode table
-	for(int j = 1; j <= ninodeblocks; j++) {
-		for (int k = 1; k <= INODES_PER_BLOCK; k++) {
-			struct fs_inode inode;
-			inode.isvalid = 0;
-			// for testing
-			if ((j == 1) && (k == 2)) {
-				inode.isvalid = 1;
-				inode.size = 52;
-			}
-			if ((j == 2) && (k == 2)) {
-				inode.isvalid = 1;
-				inode.size = 67;
-			}
-			INODE_BLOCKS[j].inode[k] = inode;
-=======
 	// clear the inode table, write null characters to every single block & byte
 	for(int j = 1; j <= ninodeblocks; j++) {
 		union fs_block curr_block;
 		disk_read(j, curr_block.data);
 		for (int k = 1; k <= INODES_PER_BLOCK; k++) {
 			curr_block.inode[k].isvalid = 0;
-			// for testing
-			if ((j == 1) && (k == 2)) {
-				curr_block.inode[k].isvalid = 1;
-				curr_block.inode[k].size = 52;
+			int data_pointers[POINTERS_PER_BLOCK];
+			for (int l = 0; l < POINTERS_PER_INODE; l++) {
+				data_pointers[l] = -1;
+				curr_block.inode[k].direct[l] = data_pointers[l];
 			}
 			if ((j == 2) && (k == 2)) {
 				curr_block.inode[k].isvalid = 1;
 				curr_block.inode[k].size = 67;
+				curr_block.inode[k].direct[0] = 123;
+				curr_block.inode[k].direct[1] = 134;
 			}
-			disk_write(j, curr_block.data);
->>>>>>> aecb7a75e1836db40a4e44686fe7d9ea620cba64
 		}
+		disk_write(j, curr_block.data);
 	}
 
 	struct fs_superblock super;
@@ -115,16 +90,12 @@ int fs_format() {
 
 	disk_write(0, SUPER_BLOCK.data);
 
-<<<<<<< HEAD
-=======
 	FORMATTED = 1;
->>>>>>> aecb7a75e1836db40a4e44686fe7d9ea620cba64
 	return 1;
 }
 
 void fs_debug()
 {
-
 	printf("superblock:\n");
 	if(SUPER_BLOCK.super.magic == FS_MAGIC) {
 		printf("    magic number is valid\n");
@@ -137,22 +108,33 @@ void fs_debug()
 
 	// inode information
 	for (int i = 1; i <= SUPER_BLOCK.super.ninodeblocks; i++) {
-<<<<<<< HEAD
-
-		// read the inode block for data
-		for (int j = 1; j <= INODES_PER_BLOCK; j++) {
-			struct fs_inode curr_inode = INODE_BLOCKS[i].inode[j];
-=======
 		union fs_block curr_block;
 		disk_read(i, curr_block.data);
 		// read the inode block for data
 		for (int j = 1; j <= INODES_PER_BLOCK; j++) {
 			struct fs_inode curr_inode = curr_block.inode[j];
->>>>>>> aecb7a75e1836db40a4e44686fe7d9ea620cba64
 			int inumber = (i-1) * INODES_PER_BLOCK + j;
-			if (curr_inode.isvalid) {
+			if (curr_inode.isvalid == 1) {
 				printf("inode %d:\n", inumber);
 				printf("    size: %d bytes\n", curr_inode.size);
+				if (curr_inode.size) {
+					const char * space = " ";
+					char direct_concat[MEM_SET];
+
+					for (int k = 0; k < POINTERS_PER_INODE; k++) {
+						if (curr_block.inode[j].direct[k] != -1) {
+							char cstr[MEM_SET];
+							sprintf(cstr, "%d", curr_block.inode[j].direct[k]);
+							// concatenate direct blocks
+							const char * src = cstr;
+							if (k > 0) {
+								strcat(direct_concat, space);
+							}
+							strcat(direct_concat, src);
+						}
+					}
+					printf("    direct blocks: %s\n", direct_concat);
+				}
 			}
 		}
 	}
@@ -160,12 +142,9 @@ void fs_debug()
 
 int fs_mount()
 {
-<<<<<<< HEAD
-=======
 	if(!FORMATTED) {
 		return 0;
 	}
->>>>>>> aecb7a75e1836db40a4e44686fe7d9ea620cba64
 	if (SUPER_BLOCK.super.magic != FS_MAGIC) { 
 		return 0;
 	}
@@ -180,48 +159,34 @@ int fs_mount()
 	for (int ib = SUPER_BLOCK.super.ninodeblocks + 1; ib < SUPER_BLOCK.super.ninodeblocks; ib++) {
 		BITMAP[ib] = 0;
 	}
-<<<<<<< HEAD
-=======
 
 	MOUNTED = 1;
->>>>>>> aecb7a75e1836db40a4e44686fe7d9ea620cba64
 	return 1;
 }
 
 int fs_create()
 {
-<<<<<<< HEAD
-=======
 	if(!MOUNTED || !FORMATTED) {
 		return 0;
 	}
->>>>>>> aecb7a75e1836db40a4e44686fe7d9ea620cba64
 	int inumber;
 	// inode information
 	for (int i = 1; i <= SUPER_BLOCK.super.ninodeblocks; i++) {
 		// read the inode block for data
-<<<<<<< HEAD
-		for (int j = 1; j <= INODES_PER_BLOCK; j++) {
-			struct fs_inode curr_inode = INODE_BLOCKS[i].inode[j];
-=======
 		union fs_block curr_block;
 		disk_read(i, curr_block.data);
 		for (int j = 1; j <= INODES_PER_BLOCK; j++) {
 			struct fs_inode curr_inode = curr_block.inode[j];
->>>>>>> aecb7a75e1836db40a4e44686fe7d9ea620cba64
 			// create inode
-			if (!curr_inode.isvalid) {
+			if (curr_inode.isvalid != 1) {
 				inumber = (i-1) * INODES_PER_BLOCK + j;
 				struct fs_inode inode;
 				inode.isvalid = 1;
 				inode.size = 0;
-<<<<<<< HEAD
-				INODE_BLOCKS[i].inode[j] = inode;
-
-=======
 				curr_block.inode[j] = inode;
 				disk_write(i, curr_block.data);
->>>>>>> aecb7a75e1836db40a4e44686fe7d9ea620cba64
+				// update bitmap
+				BITMAP[SUPER_BLOCK.super.ninodeblocks+inumber] = 1;
 				return inumber;
 			}
 		}
@@ -231,20 +196,6 @@ int fs_create()
 
 int fs_delete( int inumber )
 {
-<<<<<<< HEAD
-	int j = inumber % INODES_PER_BLOCK;
-	int i = (inumber - j) / INODES_PER_BLOCK + 1;
-
-	// on failure, return 0
-	if (!INODE_BLOCKS[i].inode[j].isvalid) {
-		return 0;
-	}
-
-	struct fs_inode inode;
-	inode.isvalid = 0;
-	inode.size = 1;
-	INODE_BLOCKS[i].inode[j] = inode;
-=======
 	if(!MOUNTED || !FORMATTED) {
 		return 0;
 	}
@@ -255,13 +206,15 @@ int fs_delete( int inumber )
 	disk_read(block, inode_block.data);
 
 	// fail if inode is invalid
-	if(!inode_block.inode[inode_in_block].isvalid) {
+	if(inode_block.inode[inode_in_block].isvalid != 1) {
 		return 0;
 	}
 
 	inode_block.inode[inode_in_block].isvalid = 0;
-	inode_block.inode[inode_in_block].size = 1;
->>>>>>> aecb7a75e1836db40a4e44686fe7d9ea620cba64
+	inode_block.inode[inode_in_block].size = 0;
+	disk_write(block, inode_block.data);
+	// update bitmap
+	BITMAP[SUPER_BLOCK.super.ninodeblocks+inumber] = 0;
 	return 1;
 }
 
@@ -277,7 +230,7 @@ int fs_getsize( int inumber )
 	disk_read(block, inode_block.data);
 	
 	// fail if inode is invalid
-	if(!inode_block.inode[inode_in_block].isvalid) {
+	if(inode_block.inode[inode_in_block].isvalid != 1) {
 		return -1;
 	}
 
@@ -315,7 +268,7 @@ int fs_write( int inumber, const char *data, int length, int offset )
 	disk_read(block, iblock.data);
 
 	// if the given inumber is invalid, return 0
-	if(!iblock.inode[inode_in_block].isvalid) {
+	if(iblock.inode[inode_in_block].isvalid != 1) {
 		return -1;
 	}
 
